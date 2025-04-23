7dc2d3829c00fe708716d032b4de2f17d4f1a1f9
Yuvakishore1001 <85071479+Yuvakishore1001@users.noreply.github.com>
2025-04-23 03:08:22 -0500

    tags updated

--- main.tf
+++ main.tf
@@ -5,12 +5,17 @@ region = "us-east-2"
 resource "aws_instance" "one"{
   ami           = "ami-0945157a116cd5d12"
   instance_type = "t2.micro"
-count = 3
+count = 2
   tags = {
-Name = "yuva_HCP-${count.index + 1}"
+Name = var.instance_tags
 }
 }
 
-resource "aws_s3_bucket" "two"{
-bucket = "tefformhcpbucket976467"
+variable "instance_tags" {
+type=map(string)
+default = {
+name = "yuva"
+server = "test"
+connection = "ssh"
+}
 }
