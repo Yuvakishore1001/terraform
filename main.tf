35e81438a374a913d1700e7a880dbe97cb013565
Yuvakishore1001 <85071479+Yuvakishore1001@users.noreply.github.com>
2025-04-23 03:14:21 -0500

    added main tags

--- main.tf
+++ main.tf
@@ -6,6 +6,9 @@ resource "aws_instance" "one"{
   ami           = "ami-0945157a116cd5d12"
   instance_type = "t2.micro"
 count = 2
+tags = {
+name = "yuva${count.index+1}"
+}
   tags = var.instance_tags
 }
 
