ee6f160e9273c2f46e379c8382897b446a453c81
Yuvakishore1001 <85071479+Yuvakishore1001@users.noreply.github.com>
2025-04-23 03:10:38 -0500

    code corrected

--- main.tf
+++ main.tf
@@ -6,10 +6,8 @@ resource "aws_instance" "one"{
   ami           = "ami-0945157a116cd5d12"
   instance_type = "t2.micro"
 count = 2
-  tags = {
-Name = var.instance_tags
-}
-}
+  tags = var.instance_tags
+}}
 
 variable "instance_tags" {
 type=map(string)
